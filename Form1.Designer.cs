5a5bed4afbac6b7a92dbf804925cbdb277d609d9
trrproject <jomgsantos@gmail.com>
2016-11-13 03:03:17 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace temporizador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+
+       
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+
+        
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.Connect = new System.Windows.Forms.Button();
+            this.Sportslist = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.nspeed = new System.Windows.Forms.TrackBar();
+            this.disconnect = new System.Windows.Forms.Button();
+            this.label_tramac = new System.Windows.Forms.Label();
+            this.label_tramar = new System.Windows.Forms.Label();
+            this.label_tramat = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nspeed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            // 
+            // textBox3
+            // 
+            resources.ApplyResources(this.textBox3, "textBox3");
+            this.textBox3.Name = "textBox3";
+            // 
+            // textBox4
+            // 
+            resources.ApplyResources(this.textBox4, "textBox4");
+            this.textBox4.Name = "textBox4";
+            // 
+            // textBox5
+            // 
+            resources.ApplyResources(this.textBox5, "textBox5");
+            this.textBox5.Name = "textBox5";
+            // 
+            // textBox6
+            // 
+            resources.ApplyResources(this.textBox6, "textBox6");
+            this.textBox6.Name = "textBox6";
+            // 
+            // textBox7
+            // 
+            resources.ApplyResources(this.textBox7, "textBox7");
+            this.textBox7.Name = "textBox7";
+            // 
+            // textBox8
+            // 
+            resources.ApplyResources(this.textBox8, "textBox8");
+            this.textBox8.Name = "textBox8";
+            // 
+            // textBox9
+            // 
+            resources.ApplyResources(this.textBox9, "textBox9");
+            this.textBox9.Name = "textBox9";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 200;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Connect
+            // 
+            resources.ApplyResources(this.Connect, "Connect");
+            this.Connect.Name = "Connect";
+            this.Connect.TabStop = false;
+            this.Connect.UseVisualStyleBackColor = true;
+            this.Connect.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // Sportslist
+            // 
+            resources.ApplyResources(this.Sportslist, "Sportslist");
+            this.Sportslist.FormattingEnabled = true;
+            this.Sportslist.Name = "Sportslist";
+            this.Sportslist.TabStop = false;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // nspeed
+            // 
+            resources.ApplyResources(this.nspeed, "nspeed");
+            this.nspeed.Maximum = 5;
+            this.nspeed.Minimum = 1;
+            this.nspeed.Name = "nspeed";
+            this.nspeed.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.nspeed.Value = 1;
+            this.nspeed.ValueChanged += new System.EventHandler(this.nspeed_Scroll);
+            this.nspeed.Scroll += new System.EventHandler(this.nspeed_Scroll);
+            // 
+            // disconnect
+            // 
+            resources.ApplyResources(this.disconnect, "disconnect");
+            this.disconnect.Name = "disconnect";
+            this.disconnect.TabStop = false;
+            this.disconnect.UseVisualStyleBackColor = true;
+            this.disconnect.Click += new System.EventHandler(this.disconnect_Click);
+            // 
+            // label_tramac
+            // 
+            resources.ApplyResources(this.label_tramac, "label_tramac");
+            this.label_tramac.Name = "label_tramac";
+            // 
+            // label_tramar
+            // 
+            resources.ApplyResources(this.label_tramar, "label_tramar");
+            this.label_tramar.Name = "label_tramar";
+            // 
+            // label_tramat
+            // 
+            resources.ApplyResources(this.label_tramat, "label_tramat");
+            this.label_tramat.Name = "label_tramat";
+            // 
+            // textBox10
+            // 
+            this.textBox10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.textBox10, "textBox10");
+            this.textBox10.Name = "textBox10";
+            // 
+            // textBox11
+            // 
+            this.textBox11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.textBox11, "textBox11");
+            this.textBox11.Name = "textBox11";
+            // 
+            // textBox12
+            // 
+            this.textBox12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.textBox12, "textBox12");
+            this.textBox12.Name = "textBox12";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.HighlightText;
+            resources.ApplyResources(this.progressBar1, "progressBar1");
+            this.progressBar1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.progressBar1.MarqueeAnimationSpeed = 15;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label_tramat);
+            this.Controls.Add(this.label_tramar);
+            this.Controls.Add(this.label_tramac);
+            this.Controls.Add(this.disconnect);
+            this.Controls.Add(this.nspeed);
+            this.Controls.Add(this.Sportslist);
+            this.Controls.Add(this.Connect);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox3);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nspeed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button Connect;
+        private System.Windows.Forms.ComboBox Sportslist;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TrackBar nspeed;
+        private System.Windows.Forms.Button disconnect;
+        private System.Windows.Forms.Label label_tramac;
+        private System.Windows.Forms.Label label_tramar;
+        private System.Windows.Forms.Label label_tramat;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+       
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO.Ports;
+using System.Threading;
+
+namespace temporizador
+{
+
+
+    public partial class Form1 : Form
+    {
+        //Importação de uma biblioteca para a verificação das teclas
+        [DllImport("user32.dll")]
+        //Função de verificação das teclas
+        static extern bool GetAsyncKeyState(System.Windows.Forms.Keys vKey);
+        //Inicialização das variáveis
+        private int slanguage = 1, trama_total = 0, trama_total_recebida = 0, trama_correcta = 0, trama_max = 5, aux = 0;
+        double percentagem = 0;
+        private string trama;
+        private string[] theSerialPortNames = SerialPort.GetPortNames();
+
+        string percento;
+
+
+        //Tramas pré-definidas
+        public byte[] Frente = new byte[] { 35, 70, 0, 51, 17 };
+        public byte[] Tras = new byte[] { 35, 084, 0, 51, 221 };
+        public byte[] Esquerda = new byte[] { 35, 83, 3, 80, 245 };
+        public byte[] Direita = new byte[] { 35, 83, 7, 115, 169 };
+        //public byte[] Centro = new byte[] { 035, 083, 005, 225 };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Procura das portas de série disponíveis
+            for (int i = 0; i < theSerialPortNames.Length; i++)
+                Sportslist.Items.Add(theSerialPortNames[i]);
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Direcção de cima
+            if (GetAsyncKeyState(Keys.Up) && (!GetAsyncKeyState(Keys.Left)) && (!GetAsyncKeyState(Keys.Right)) && (!GetAsyncKeyState(Keys.Down)))
+            {
+
+                textBox2.BackColor = Color.Black;
+                textBox1.BackColor = Color.Empty; textBox3.BackColor = Color.Empty; textBox4.BackColor = Color.Empty; textBox5.BackColor = Color.Empty;
+                textBox6.BackColor = Color.Empty; textBox7.BackColor = Color.Empty; textBox8.BackColor = Color.Empty; textBox9.BackColor = Color.Empty;
+                
+                serialPort1.Write(Frente, 0, trama_max);
+                trama_total += 1;
+                //this.Invoke(new EventHandler(Update_total));
+            }
+            //Direcção de cima e esquerda
+            else if (GetAsyncKeyState(Keys.Up) && (GetAsyncKeyState(Keys.Left)) && (!GetAsyncKeyState(Keys.Right)) && (!GetAsyncKeyState(Keys.Down)))
+            {
+                textBox1.BackColor = Color.Black;
+                textBox2.BackColor = Color.Empty; textBox3.BackColor = Color.Empty; textBox4.BackColor = Color.Empty; textBox5.BackColor = Color.Empty;
+                textBox6.BackColor = Color.Empty; textBox7.BackColor = Color.Empty; textBox8.BackColor = Color.Empty; textBox9.BackColor = Color.Empty;
+                
+                serialPort1.Write(Esquerda, 0, trama_max);
+                trama_total += 1;
+                Thread.Sleep(2);
+                serialPort1.Write(Frente, 0, trama_max);
+                trama_total += 1;
+                //this.Invoke(new EventHandler(Update_total));
+            }
+            //Direcção de cima e direita
+            else if (GetAsyncKeyState(Keys.Up) && (!GetAsyncKeyState(Keys.Left)) && (GetAsyncKeyState(Keys.Right)) && (!GetAsyncKeyState(Keys.Down)))
+            {
+                textBox3.BackColor = Color.Black;
+                textBox1.BackColor = Color.Empty; textBox2.BackColor = Color.Empty; textBox4.BackColor = Color.Empty; textBox5.BackColor = Color.Empty;
+                textBox6.BackColor = Color.Empty; textBox7.BackColor = Color.Empty; textBox8.BackColor = Color.Empty; textBox9.BackColor = Color.Empty;
+                
+                serialPort1.Write(Direita, 0, trama_max);
+                trama_total += 1;
+                Thread.Sleep(2);
+                serialPort1.Write(Frente, 0, trama_max);
+                trama_total += 1;
+                //this.Invoke(new EventHandler(Update_total));
+            }
+            //Direcção de baixo
+            else if (!GetAsyncKeyState(Keys.Up) && (!GetAsyncKeyState(Keys.Left)) && (!GetAsyncKeyState(Keys.Right)) && (GetAsyncKeyState(Keys.Down)))
+            {
+                textBox8.BackColor = Color.Black;
+                textBox1.BackColor = Color.Empty; textBox2.BackColor = Color.Empty; textBox4.BackColor = Color.Empty; textBox5.BackColor = Color.Empty;
+                textBox6.BackColor = Color.Empty; textBox7.BackColor = Color.Empty; textBox3.BackColor = Color.Empty; textBox9.BackColor = Color.Empty;
+                serialPort1.Write(Tras, 0, trama_max);
+                trama_total += 1;
+                //this.Invoke(new EventHandler(Update_total));
+            }
+            //Direcção de baixo e esquerda
+            else if (!GetAsyncKeyState(Keys.Up) && (GetAsyncKeyState(Keys.Left)) && (!GetAsyncKeyState(Keys.Right)) && (GetAsyncKeyState(Keys.Down)))
+            {
+                textBox7.BackColor = Color.Black;
+                textBox1.BackColor = Color.Empty; textBox2.BackColor = Color.Empty; textBox3.BackColor = Color.Empty; textBox4.BackColor = Color.Empty; textBox5.BackColor = Color.Empty;
+                textBox6.BackColor = Color.Empty; textBox8.BackColor = Color.Empty; textBox9.BackColor = Color.Empty;
+                
+                serialPort1.Write(Esquerda, 0, trama_max);
+                trama_total += 1;
+                Thread.Sleep(2);
+                serialPort1.Write(Tras, 0, trama_max);
+                trama_total += 1;
+                //this.Invoke(new EventHandler(Update_total));
+            }
+            //Direcção de baixo e direita
+            else if (!GetAsyncKeyState(Keys.Up) && (!GetAsyncKeyState(Keys.Left)) && (GetAsyncKeyState(Keys.Right)) && (GetAsyncKeyState(Keys.Down)))
+            {
+                textBox9.BackColor = Color.Black;
+                textBox1.BackColor = Color.Empty; textBox2.BackColor = Color.Empty; textBox3.BackColor = Color.Empty; textBox4.BackColor = Color.Empty; textBox5.BackColor = Color.Empty;
+                textBox6.BackColor = Color.Empty; textBox7.BackColor = Color.Empty; textBox8.BackColor = Color.Empty;
+                
+                serialPort1.Write(Direita, 0, trama_max);
+                trama_total += 1;
+                Thread.Sleep(2);
+                serialPort1.Write(Tras, 0, trama_max);
+                trama_total += 1;
+                //this.Invoke(new EventHandler(Update_total));
+            }
+            //Direcção de esquerda
+            else if (!GetAsyncKeyState(Keys.Up) && (GetAsyncKeyState(Keys.Left)) && (!GetAsyncKeyState(Keys.Right)) && (!GetAsyncKeyState(Keys.Down)))
+            {
+                textBox4.BackColor = Color.Black;
+                textBox1.BackColor = Color.Empty; textBox2.BackColor = Color.Empty; textBox3.BackColor = Color.Empty; textBox5.BackColor = Color.Empty;
+                textBox6.BackColor = Color.Empty; textBox7.BackColor = Color.Empty; textBox8.BackColor = Color.Empty; textBox9.BackColor = Color.Empty;
+                
+                serialPort1.Write(Esquerda, 0, trama_max);
+                trama_total += 1;
+                //this.Invoke(new EventHandler(Update_total));
+            }
+            ///////Diecções de Direita
+            else if (!GetAsyncKeyState(Keys.Up) && (!GetAsyncKeyState(Keys.Left)) && (GetAsyncKeyState(Keys.Right)) && (!GetAsyncKeyState(Keys.Down)))
+            {
+                textBox6.BackColor = Color.Black;
+                textBox1.BackColor = Color.Empty; textBox2.BackColor = Color.Empty; textBox3.BackColor = Color.Empty; textBox4.BackColor = Color.Empty; textBox5.BackColor = Color.Empty;
+                textBox7.BackColor = Color.Empty; textBox8.BackColor = Color.Empty; textBox9.BackColor = Color.Empty;
+                
+                serialPort1.Write(Direita, 0, trama_max);
+                trama_total += 1;
+                //this.Invoke(new EventHandler(Update_total));
+            }
+            //Nenhuma direcção associada
+            else
+            {
+                textBox5.BackColor = Color.Black;
+                textBox1.BackColor = Color.Empty; textBox2.BackColor = Color.Empty; textBox3.BackColor = Color.Empty; textBox4.BackColor = Color.Empty;
+                textBox6.BackColor = Color.Empty; textBox7.BackColor = Color.Empty; textBox8.BackColor = Color.Empty; textBox9.BackColor = Color.Empty;
+            }
+        }
+        //Contador para a velocidade (mais ou menos) 
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (GetAsyncKeyState(Keys.A))
+                if (nspeed.Value < nspeed.Maximum)
+                    nspeed.Value = nspeed.Value + 1;
+            if (GetAsyncKeyState(Keys.Z))
+                if (nspeed.Value > nspeed.Minimum)
+                    nspeed.Value = nspeed.Value - 1;
+        }
+
+        //Botão para ligar a uma porta de série
+        private void Connect_Click(object sender, EventArgs e)
+        {
+            if (theSerialPortNames.Length == 0)
+            {
+                MessageBox.Show("Não existem portas de série neste computador");
+            }
+            else
+            {
+                if ((Sportslist.Text == "Escolha uma porta") || (Sportslist.Text == ""))
+                    MessageBox.Show("Tem de escolher uma porta");
+                else
+                {
+                    serialPort1.PortName = Sportslist.Text;
+                    try
+                    {
+                        serialPort1.Open();
+                        Sportslist.Enabled = false;
+                        timer1.Start();
+                        timer2.Start();
+                        Connect.Enabled = false;
+                        disconnect.Enabled = true;
+                    }
+                    catch (Exception a)
+                    {
+                        MessageBox.Show("Não é possível estabelecer ligação à porta: " + serialPort1.PortName.ToString());
+                    }
+                }
+            }
+        }
+        //Aceleração
+        private void nspeed_Scroll(object sender, EventArgs e)
+        {
+            Frente[3] = (byte)(51 * nspeed.Value);
+            aux = Frente[0] + Frente[1] + Frente[2] + Frente[3];
+            Frente[4] = CRC8x8((byte)aux);
+
+            Tras[3] = (byte)(51 * nspeed.Value);
+            aux = Tras[0] + Tras[1] + Tras[2] + Tras[3];
+            Tras[4] = CRC8x8((byte)aux);
+
+        }
+        //Recepção de dados
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+                this.Invoke(new EventHandler(DoUpdate));
+        }
+        //Actualização da caixas de texto
+        private void DoUpdate(object s, EventArgs e)
+        {
+            trama = serialPort1.ReadExisting();
+            if (trama.Contains('S'))
+            {
+                trama_total_recebida += 1;
+                textBox11.Text = trama_total_recebida.ToString();
+            }
+            if (trama.Contains('N'))
+            {
+                trama_correcta += 1;
+                textBox10.Text = trama_correcta.ToString();
+            }
+            textBox12.Text = trama_total.ToString();
+            label3.Text = Math.Round(100 - ((Division(trama_correcta, trama_total) * 100)), 3).ToString() + "%";
+            progressBar1.Value = (int)((Division(trama_correcta, trama_total) * 100));
+            //this.Invoke(new EventHandler(Update_total));
+        }
+
+        private void Update_total(object myobject, EventArgs e)
+        {
+            textBox12.Text = trama_total.ToString();
+            label3.Text = Math.Round(Division(trama_total, trama_correcta), 3).ToString();
+            progressBar1.Value = (int)Math.Round(Division(trama_total, trama_correcta), 3);
+        }
+        //Desliga-se
+        private void disconnect_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+                timer1.Stop();
+            if (timer2.Enabled)
+                timer2.Stop();
+            if (serialPort1.IsOpen)
+                serialPort1.Close();
+            Connect.Enabled = true;
+            Sportslist.Enabled = true;
+            disconnect.Enabled = false;
+        }
+
+        private byte CRC8(byte input, byte seed)
+        {
+            byte i;
+            byte feedback;
+
+            for (i = 0; i < 8; i++)
+            {
+                feedback = (byte)((seed ^ input) & 0x01);
+                if (feedback == 0)
+                    seed >>= 1;
+                else
+                {
+                    seed ^= 0x18;
+                    seed >>= 1;
+                    seed |= 0x80;
+                }
+                input >>= 1;
+            }
+
+            return seed;
+        }
+
+        private byte CRC8x8(byte input)
+        {
+            byte i;
+            byte check;
+
+            check = 0;
+            for (i = 0; i < 8; i++)
+            {
+                check = CRC8(input, check);
+                input++;
+            }
+
+            return check;
+        }
+
+        public double Division(int a, int b) 
+        {
+            if ((b == 0) || (a == b)) 
+                return (double)0;
+            else
+                return (double)a / b; 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            slanguage = 1;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            slanguage = 0;
+            Form.ActiveForm.Refresh();
+
+        }
+    }
+}
